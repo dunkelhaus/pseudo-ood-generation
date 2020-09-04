aec72481dcc5fe89a34610a1895e48e5c935f316
Suraj Jena <jena.suraj.k@gmail.com>
2020-09-03 22:52:38 -0700

    Rudimentary encoder code

---
+++ components/encoder.py
@@ -0,0 +1,40 @@
+import torch
+import allennlp
+import numpy as np
+from allennlp.modules.token_embedders import TokenEmbedder
+from allennlp.modules import Seq2VecEncoder, LSTMSeq2VecEncoder
+
+
+class POGEncoder(LSTMSeq2VecEncoder):
+
+    def __init__(
+            self,
+            input_size: int,
+            hidden_size: int,
+            embedder: TokenEmbedder,
+            num_layers: int = 1,
+            bias: bool = True,
+            dropout: float = 0.0,
+            bidirectional: bool = False
+    ):
+        super().__init__(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            bias=bias,
+            dropout=dropout,
+            bidirectional=bidirectional
+        )
+        self.embedder = embedder
+
+    def forward(
+            self,
+            sentence: TextFieldTensors,
+            hidden: Any
+    ) -> Dict[str, torch.Tensor]:
+        # Step 2
+        _xi = self.embedder(sentence)
+        xi = _xi
+        xi, hidden = self.module(xi, hidden)
+
+        return xi, hidden
