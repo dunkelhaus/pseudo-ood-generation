41d526df1c25d2c99cb70c1f06d421fa9e2bcfde
Suraj Jena <jena.suraj.k@gmail.com>
2021-01-17 21:39:32 -0800

    Cleaned POG builder fns

--- builders.py
+++ builders.py
@@ -1,17 +1,16 @@
-import torch
 import logging.config
+from typing import Any, Tuple
 from allennlp.models import Model
+from allennlp.data import DataLoader
 from allennlp.data import Vocabulary
-from allennlp.data import PyTorchDataLoader
-from allennlp.data.dataloader import TensorDict
-from datasets.readers.oos_eval import OOSEvalReader
-from allennlp.data import DatasetReader, DataLoader, Instance
-from models.bert_linear_classifier import BertLinearClassifier
+from allennlp.training.trainer import Trainer
+from models.builders import build_epoch_callbacks
+from models.builders import build_batch_callbacks
+from allennlp.training.trainer import GradientDescentTrainer
+from models.bert_linear.classifier import BertLinearClassifier
 from allennlp.training.optimizers import HuggingfaceAdamWOptimizer
-from typing import Any, List, Dict, Tuple, Value, Iterable, Optional
-from allennlp.modules.seq2vec_encoders.bert_pooler import BertPooler
-from allennlp.training.trainer import Trainer, GradientDescentTrainer
 from allennlp.modules.text_field_embedders import TextFieldEmbedder
+from allennlp.modules.seq2vec_encoders.bert_pooler import BertPooler
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import PretrainedTransformerEmbedder
 
@@ -19,42 +18,34 @@ from allennlp.modules.token_embedders import PretrainedTransformerEmbedder
 log = logging.getLogger(__name__)
 
 
-def build_dataset_reader() -> DatasetReader:
+def bert_linear_builders(
+        serialization_dir: str,
+        train_loader: DataLoader,
+        dev_loader: DataLoader,
+        lr: float,
+        num_epochs: int,
+        vocab: Vocabulary,
+        wbrun: Any
+) -> Tuple[Model, Trainer]:
     """
-    Instantiate dataset reader - factory method.
+    Simple wrapper for both model-specific builder fns.
 
-    :return OOSEvalReader: Instantiated DatasetReader object.
+    :returns Model, Trainer: The Model & Trainer objects
+                                respectively.
     """
-    return OOSEvalReader()
+    model = build_model(vocab, wbrun)
 
+    trainer = build_trainer(
+        model,
+        serialization_dir,
+        train_loader,
+        dev_loader,
+        lr=lr,
+        num_epochs=num_epochs,
+        wbrun=wbrun
+    )
 
-def build_epoch_callbacks(wbrun) -> EpochCallback:
-    """
-    Instantiate callback - factory method.
-
-    :return LogMetricsToWandb: Instantiated LogMetricsToWandb object.
-    """
-    return [LogMetricsToWandb(wbrun=wbrun)]
-
-
-def build_batch_callbacks(wbrun) -> BatchCallback:
-    """
-    Instantiate callback - factory method.
-
-    :return LogBatchMetricsToWandb: Instantiated LogBatchMetricsToWandb object.
-    """
-    return [LogBatchMetricsToWandb(wbrun=wbrun)]
-
-
-def build_vocab(instances: Iterable[Instance]) -> Vocabulary:
-    """
-    Build the Vocabulary object from the instances.
-
-    :param instances: Iterable of allennlp instances.
-    :return Vocabulary: The Vocabulary object.
-    """
-    log.debug("Building the vocabulary.")
-    return Vocabulary.from_instances(instances)
+    return model, trainer
 
 
 def build_model(vocab: Vocabulary, wbrun: Any) -> Model:
@@ -82,71 +73,18 @@ def build_model(vocab: Vocabulary, wbrun: Any) -> Model:
     encoder = BertPooler("bert-base-uncased", requires_grad=True)
     # encoder = PytorchSeq2VecWrapper(torch.nn.LSTM(768,20,batch_first=True))
     log.debug("Encoder built.")
+
     return BertLinearClassifier(vocab, embedder, encoder, wbrun).cuda(0)
 
 
-def build_data_loader(
-    data: torch.utils.data.Dataset,
-    batch_size: int,
-    shuffle: bool = True
-) -> DataLoader:
-    """
-    Build an AllenNLP DataLoader.
-
-    :param train_data: The training dataset, torch object.
-    :param dev_data: The dev dataset, torch object.
-    :return train_loader, dev_loader: The train and dev data loaders as a
-            tuple.
-    """
-    # Note that DataLoader is imported from allennlp above, *not* torch.
-    # We need to get the allennlp-specific collate function, which is
-    # what actually does indexing and batching.
-    log.debug("Building DataLoader.")
-    loader = PyTorchDataLoader(
-        data,
-        batch_size=batch_size,
-        shuffle=True
-    )
-    log.debug("DataLoader built.")
-    return loader
-
-
-def build_train_data_loaders(
-    train_data: torch.utils.data.Dataset,
-    dev_data: torch.utils.data.Dataset,
-    batch_size: int
-) -> Tuple[DataLoader, DataLoader]:
-    """
-    Build the AllenNLP DataLoaders.
-
-    :param train_data: The training dataset, torch object.
-    :param dev_data: The dev dataset, torch object.
-    :return train_loader, dev_loader: The train and dev data loaders as a
-            tuple.
-    """
-    log.debug("Building Training DataLoaders.")
-    train_loader = build_data_loader(
-        train_data,
-        batch_size=batch_size,
-        shuffle=True
-    )
-    dev_loader = build_data_loader(
-        dev_data,
-        batch_size=batch_size,
-        shuffle=False
-    )
-    log.debug("Training DataLoaders built.")
-    return train_loader, dev_loader
-
-
 def build_trainer(
-    model: Model,
-    serialization_dir: str,
-    train_loader: DataLoader,
-    dev_loader: DataLoader,
-    lr: float,
-    num_epochs: int,
-    wbrun: Any
+        model: Model,
+        serialization_dir: str,
+        train_loader: DataLoader,
+        dev_loader: DataLoader,
+        lr: float,
+        num_epochs: int,
+        wbrun: Any
 ) -> Trainer:
     """
     Build the model trainer. Includes instantiating the optimizer as well.
@@ -173,4 +111,5 @@ def build_trainer(
         epoch_callbacks=build_epoch_callbacks(wbrun),
         batch_callbacks=build_batch_callbacks(wbrun)
     )
+
     return trainer
