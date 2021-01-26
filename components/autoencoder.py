c1028dfa11c47e53826bd0b9ba15c682d9c5800a
Suraj Jena <jena.suraj.k@gmail.com>
2021-01-25 17:42:30 -0800

    Implemented encoder and decoder based on pytorch setup

---
+++ components/autoencoder.py
@@ -0,0 +1,28 @@
+import torch
+import numpy as np
+from torch import nn
+
+
+class POGEncoder(nn.Module):
+    """
+    LSTM encoder.
+    Adds noise too.
+    """
+    def __init__(self):
+        super(POGEncoder, self).__init__()
+        self.lstm = nn.LSTM(768, 100)
+
+    def forward(self, x):
+        z = self.lstm(x)
+        epsilon = np.random.normal(size=z.shape)
+        return z + epsilon
+
+
+class POGDecoder(torch.nn.Module):
+    def __init__(self):
+        super(POGDecoder, self).__init__()
+        self.lstm = nn.LSTM(100, 768)
+
+    def forward(self, z, x):
+        pred_vec = nn.functional.log_softmax(self.lstm(z))
+        return pred_vec, nn.functional.nll_loss(pred_vec, x)
